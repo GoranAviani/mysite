806c8a93fc0b9fbd286aed5acd7468a7e735ea52
goranaviani <goranaviani@gmail.com>
2019-01-25 17:44:39 +0100

    poast model

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-STATUS_CHOICES = (
-('draft', 'Draft'),
-('published', 'Published'),
-)
-title = models.CharField(max_length=250)
+  #  statusChoices = ('draft', 'Draft'),('published', 'Published'),)
+    title = models.CharField(max_length=250)
+    slug  = models.SlugField(max_length = 250, unique_for_date = 'publish')
+    author = models.ForeignKey(User, on_delete = models.CASCADE),
+    body = models.TextField()
+    publish = models.DateTimeField(default = timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+ #   status = models.CharField(max_length = 10, choices = statusChoices, defailt = 'draft')
+
+    class Meta:
+        ordering = ('-publish',)
+    def __str__(self):
+        return self.title
